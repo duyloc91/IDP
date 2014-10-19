7e654a53ee37bf501656ce947e7cd331a3f07f0f
gjh1989 <goh.junhong@gmail.com>
2014-10-19 18:17:53 +0800

    recommit of firebase + notification.html

--- www/js/my-angularfire.js
+++
@@ -1,7 +0,0 @@
-function add(username, password) {
-    var myFirebaseRef = new Firebase("https://boiling-torch-6781.firebaseio.com/users");
-    myFirebaseRef.push({
-        username: username,
-        password: password
-    });
-}
